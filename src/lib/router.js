4e8de47a4311cbec57a13cd470b962035000d9f3
Daniela Castro Salazar <88462620+DaniCastro-developer@users.noreply.github.com>
2021-11-05 15:46:05 -0300

    arreglos slint

--- src/lib/router.js
+++ src/lib/router.js
@@ -4,25 +4,25 @@ import { createCount } from './view/templateCreateCount.js';
 const showTemplete = (hash) => {
   const containerRoot = document.getElementById('root');
   containerRoot.innerHTML = '';
-  
+
   switch (hash) {
     case '#/login':
-        containerRoot.appendChild(homeLogin());
-            break;
-        case '#/count':
-            containerRoot.appendChild(createCount());
-            break;
-        default:
-            containerRoot.innerHTML = `<h2> no found </h2>`
-    }
-}
+      containerRoot.appendChild(homeLogin());
+      break;
+    case '#/count':
+      containerRoot.appendChild(createCount());
+      break;
+    default:
+      containerRoot.innerHTML = '`<h2> no found </h2>`';
+  }
+};
 
 export const changeRoute = (hash) => {
   if (hash === '#/count') {
     return showTemplete(hash);
-} else if (hash === '#/login') {
+  } else if (hash === '#/login') {
     return showTemplete(hash);
-} else {
-  return showTemplete(hash);
-    }
-}
+  } else {
+    return showTemplete(hash);
+  }
+};
--- src/main.js
+++ src/main.js
@@ -1,17 +1,15 @@
 import { myFunction } from './lib/index.js';
 import { homeLogin } from './lib/view/templateLogin.js';
 import { changeRoute } from './lib/router.js';
-import { createCount } from './lib/view/templateCreateCount.js';
 
 document.getElementById('root').appendChild(homeLogin());
 
 const init = () => {
-
-    window.addEventListener('hashchange', () => {
-        myFunction();
-        console.log(window.location.hash);
-        changeRoute(window.location.hash)
-    })
-}
+  window.addEventListener('hashchange', () => {
+    myFunction();
+    console.log(window.location.hash);
+    changeRoute(window.location.hash);
+  });
+};
 
 window.addEventListener('load', init);
